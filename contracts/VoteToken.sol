5c9f3265d3f318eca84a819e16a22cb52e8d043f
Ari <arinzeahmed@gmail.com>
2025-11-10 15:36:05 +0100

    Update VoteToken.sol

--- contracts/VoteToken.sol
+++ contracts/VoteToken.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title VoteToken - Governance token with voting power
+/// @author Your Name
+contract VoteToken is ERC20Votes, Ownable {
+    constructor() ERC20("VoteToken", "VOTE") ERC20Permit("VoteToken") {
+        _mint(msg.sender, 1_000_000 * 1e18);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
